4acd95ce96e11bec61edc56390d11bb8d1cfe5bf
Maxime CORTYL <maxime.cortyl@smile.fr>
2020-03-08 21:52:24 +0100

    Add Result Page

--- src/index.jsx
+++ src/index.jsx
@@ -22,9 +22,11 @@ function InitPage(attributes) {
   const pageLayout = new StackLayout({alignment: 'stretchX', spacing: 16});
   return(
     <Page padding={20} centerY layout={pageLayout} {...attributes}>
-      <ImageView center image='src/img/logo.png' />
-      <TextView font='24px'>Welcome to the FeedExperts app that help you take the correct descisions when it comes to using Tonisity</TextView>
-      <Button onSelect={() => getQuestionPage()}>Start</Button>
+      <ScrollView layoutData='stretchY' layout={pageLayout}>
+        <ImageView center image='src/img/logo.png' />
+        <TextView font='24px'>Welcome to the Tonisity app that help you take the correct descisions when it comes to using our product</TextView>
+        <Button onSelect={() => getQuestionPage()}>Start</Button>
+      </ScrollView>
     </Page>
   )
 }
@@ -34,10 +36,24 @@ function QuestionPage(attributes) {
   const pageLayout = new StackLayout({alignment: 'stretchX'});
   return(
     <Page padding={20} layout={pageLayout} {...attributes}>
-      <TextView font='24px'></TextView>
-      <TextView font='24px' text={questions[questionId]}></TextView>
-      <Button centerX value={questionId} onSelect={(event) => showResult(event, 0)}>Yes</Button>
-      <Button centerX value={questionId} onSelect={(event) => showResult(event, 1)}>No</Button>
+      <ScrollView layoutData='stretchY' layout={pageLayout}>
+        <TextView font='24px'></TextView>
+        <TextView font='24px' text={questions[questionId]}></TextView>
+        <Button centerX value={questionId} onSelect={(event) => showResult(event, 0)}>Yes</Button>
+        <Button centerX value={questionId} onSelect={(event) => showResult(event, 1)}>No</Button>
+      </ScrollView>
+    </Page>
+  )
+}
+
+/** @param {tabris.Attributes<Page>} attributes */
+function ReusultPage(attributes) {
+  const pageLayout = new StackLayout({alignment: 'stretchX'});
+  return(
+    <Page padding={20} layout={pageLayout} {...attributes}>
+      <ScrollView layoutData='stretchY' layout={pageLayout}>
+        <TextView font='24px' text={getUserAnswers()}></TextView>
+      </ScrollView>
     </Page>
   )
 }
@@ -50,6 +66,12 @@ function getQuestionPage() {
   );
   textViews = contentView.find(TextView);
   buttons = contentView.find(Button);
+}
+
+function getResultPage() {
+  navigationView.append(
+    <ReusultPage title={'Result Page'} />
+  );
 }
 
 function resetData() {
@@ -68,6 +90,11 @@ function getNextStep(questionId) {
 function showResult(event, answer) {  
   questionId = event.target.value;
 
+  if (questionId === null) {
+    getResultPage();
+    return;
+  } 
+
   setUserAnswer(questionId, answer);
   let nextStep = getNextStep(questionId);
 
@@ -79,13 +106,29 @@ function showResult(event, answer) {
 
   if (questionId === null){
     //Remove buttons
-    console.log(buttons);
-    buttons[1].dispose();
+    buttons[1].text = 'Show your results';
     buttons[2].dispose();
   }
 
   textViews[1].text = suggestion;
   textViews[2].text = question;
+}
+
+function getUserAnswers() {
+  var userAnswers = '';
+  answers.forEach(function(element, key) {
+    if(element != null) {
+      let nextStep = getNextStep(key);
+      let question = questions[key];
+      let suggestion = nextStep[element+2] === null ? '' : suggestions[nextStep[element+2]];
+      let answer = element === 0 ? 'Yes' : 'No';
+      
+      userAnswers = userAnswers + question + '\n' + answer + '\n\n';
+      suggestion != '' ? userAnswers = userAnswers + suggestion + '\n' : userAnswers = userAnswers;
+    }
+  });
+
+  return userAnswers;
 }
 
 /** INIT FUNCTIONS */
@@ -98,7 +141,7 @@ function initQuestions() {
     'Do you have any pre-weaning mortality issues?',
     'Do you have satisfactory weaning weights?',
     'Are newly-weaned pigs expereriencing transport stress?',
-    'Do you have issues with getting newly-weaning pigs started on feed or water?',
+    'Do you have issues with getting newly-weaned pigs started on feed or water?',
     'Do you have issues with post-weaning diarrhea?',
     'Is milk replacer used in farrowing house?',
     'Is creep feed available in farrowing house?',
@@ -113,7 +156,7 @@ function initSuggestions() {
     'Use Tonisity Px instead of milk replacer in 1st week with Tonisity PX - 3% solution - 500mL/Litter/day - days 2-8 of age',
     'Use Tonisity Px - 3% solution - 500mL/Litter on top of creep feed - for the 3 days before weaning',
     'Use Tonisity Px - 3% solition - 20mL solution per 100g creep feed - for 3 days at initiation of creep feeding then Use Tonisity Px - 3%, Solution - 500mL/Litter on top of creep feed - for the 3 days before weaning',
-    'Use Tonisity Px - 3% solition - 20mL solution per 100g sow\'s feed - for 3 days at initiation of sow\'s feeding then Use Tonisity Px - 3%, Solution - 500mL/Litter on top of creep feed - for the 3 days before weaning',
+    'Use Tonisity Px - 3% solition - 20mL solution per 100g sow\'s feed - for 3 days at initiation of sow\'s feeding then Use Tonisity Px - 3%, Solution - 500mL/Litter on top of sow\'s feed - for the 3 days before weaning',
     'Congratulations!!!',
     'Use Tonisity Px - 3% solution - Up to 100 mL/pig/day on top of creep feed or starter feed',
     'Use Tonisity PxW - using medicator pump - Deliver through water lines using 5 days tapering dose on arrival in post-weaning barn'
